67feb9a9dea540ee23100f772c626996a933c424
Danilo Saggiomo Meguro <danilo.meguro@gmail.com>
2025-11-24 15:02:44 -0300

    Frontend
    Frontend desenvolvido por terceiros atualizado no repositório

---
+++ frontend/suco2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Suco 2</title>
+</head>
+<body>
+    <h1>Suco 2</h1>
+    <p>Informações do Suco 2...</p>
+    <a href="menu.php"><button>Voltar ao Menu</button></a>
+</body>
+</html>
